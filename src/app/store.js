c7a342ca32ccac211d41f97e660028c058bfa8a0
ClaytonDewey <clay@dryadmedia.com>
2023-07-01 14:25:52 -0500

    Export topicsReducer

--- src/app/store.js
+++ src/app/store.js
@@ -2,5 +2,7 @@ import { configureStore } from '@reduxjs/toolkit';
 import topicsReducer from '../features/topics/topicsSlice';
 
 export default configureStore({
-  reducer: {},
+  reducer: {
+    topics: topicsReducer,
+  },
 });
